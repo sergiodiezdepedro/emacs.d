a5e5d91ddc0550052fc19eeb85362bb84e576d9d
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2016-09-27 01:00:10 +0200

    Añadido undo-tree mode

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # emacs.d
 Mi configuración de Emacs. No es de gran valor, ya que soy un usuario muy primerizo. 
 
-Este repositorio es una forma de mantener sicronizada la configuración del editor en mis ordenadores.
+Este repositorio es una forma de mantener sincronizada la configuración del editor en mis ordenadores.
--- init.el
+++ init.el
@@ -166,3 +166,11 @@
 
 ;; Definir F8 para ver la imagen en una etiqueta <img>
 (global-set-key (kbd "<f8>") 'find-file-at-point)
+
+;; undo-tree-mode
+(global-undo-tree-mode 1)
+;; make ctrl-z undo
+(global-set-key (kbd "C-z") 'undo)
+;; make ctrl-Z redo
+(defalias 'redo 'undo-tree-redo)
+(global-set-key (kbd "C-S-z") 'redo)
